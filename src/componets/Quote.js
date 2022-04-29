6916cfd26d71c50d19db71725d24e52d59f24897
aleconiglio16 <alejandroconiglio16@gmail.com>
2022-04-29 04:25:46 -0300

    first commit

---
+++ src/componets/Quote.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Quote = ({quote}) => {
+
+    return (
+        <p>
+             { quote.text } <br/>
+            <span>-{ quote.author } </span>
+      </p>
+    )
+}
+
+export default Quote
